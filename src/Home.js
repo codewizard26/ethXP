c7cc1e017fd01e321bb1c22565c10a8b80cd3f4c
codewizard26 <nikhilmishra2608@gmail.com>
2023-12-09 04:31:23 +0530

    added feature for upload

--- src/Home.js
+++ src/Home.js
@@ -2,19 +2,10 @@
 
 import React, {useRef} from 'react';
 import researchPaperImage from './research.svg';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
 
-    const fileInputRef = useRef(null);
-
-    const handleFileChange = (e) => {
-        const file = e.target.files[0];
-        console.log('Selected file:', file);
-      };
-    
-      const handleResearch = () => {
-        fileInputRef.current.click(); // Trigger click event on file input
-      };
 
   return (
     <div className="bg-gray-900 min-h-screen flex items-center justify-center text-white">
@@ -23,15 +14,9 @@ const Home = () => {
           <p className="text-lg mb-8 ">
             Welcome to our platform where you can upload and manage your research papers in a completely decentralized way no hustle of waiting for so long to get your work published and verified with the power of blockchain to own and ai to check uniqueness and relations
           </p>
-          <button onClick={handleResearch} className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-            Upload Research
+          <button  className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+           <Link to="/upload">Upload Research</Link> 
           </button>
-          <input
-            ref={fileInputRef}
-            type="file"
-            className="hidden"
-            onChange={handleFileChange}
-          />
           <button className="bg-blue-500 ml-4 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
             Review Research
           </button>
--- src/lighthouse/upload.js
+++ src/lighthouse/upload.js
@@ -1,10 +1,20 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import lighthouse from "@lighthouse-web3/sdk";
 
 function UploadL() {
   const [file, setFile] = useState(null);
-  const [cId, setId] = useState(null);
+  const [cId, setId] = useState("");
   const apiKey = "535dec75.c7a1a9d86768459ead1ccac234bcb89b";
+
+  useEffect(() => {
+    if (cId !== "") {
+      console.log("CID has updated:", cId);
+      shareFile(); // Move shareFile call inside useEffect
+    } else {
+      console.log("CID not obtained yet");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cId]);
 
   const signAuthMessage = async () => {
     if (window.ethereum) {
@@ -45,7 +55,13 @@ function UploadL() {
         return;
       }
 
+      console.log(encryptionAuth, "above")
+
+
       const { signature, signerAddress } = encryptionAuth;
+
+      console.log(signature, "sign below")
+
 
       const output = await lighthouse.uploadEncrypted(
         file,
@@ -56,10 +72,11 @@ function UploadL() {
       );
       console.log("Encrypted File Status:", output);
 
+      
+
       if (output && output.data && output.data[0] && output.data[0].Hash) {
-        setId(output.data[0].Hash);
+        setId(`${output.data[0].Hash}`);
         console.log(`CID: ${output.data[0].Hash}`);
-        shareFile(); // Move shareFile call here
       } else {
         console.error("Invalid CID obtained.");
       }
@@ -69,12 +86,13 @@ function UploadL() {
   };
 
   const shareFile = async () => {
+    console.log(cId,"chla")
     try {
       // Check if CID is available before proceeding
       // Rest of your code for sharing the file
       // Ensure 'cId' has the correct CID value
 
-      const cid = "QmSaYCCvKBXeD9zACRVf7QexH7NFM5yfdi8hwLgwUcdsr7" // Use the obtained CID
+      const cid = cId // Use the obtained CID
       const publicKey = '0x0313922d9E9243B12f6FF1172b965fA8FF8bd31F';
       const signedMessage = "SIGNATURE";
       const publicKeyUserB = ["0x279317E79E6533837b52B117875C717fAb9453AA"];
@@ -103,10 +121,42 @@ function UploadL() {
 
   return (
     <div className="App">
-      <input type="file" onChange={handleFileChange} />
-      <button onClick={uploadEncryptedFile} disabled={!file}>
+        <div className="flex min-h-screen items-center justify-center bg-gray-900 text-white">
+      <div className="max-w-4xl mx-auto p-8">
+        <h1 className="text-4xl font-bold mb-8 text-center">Hello Researcher</h1>
+        
+        <div className="flex flex-col md:flex-row items-center justify-center md:justify-between">
+          {/* File Upload */}
+          <div className="mb-6 md:mb-0 md:mr-4 flex items-center">
+            
+            <input type="file" onChange={handleFileChange} />
+            <button onClick={uploadEncryptedFile} disabled={!file}>
         Upload Encrypted File
       </button>
+          </div>
+
+          {/* SVG on the right */}
+          <div className="flex-shrink-0">
+            <svg
+              className="h-32 w-32 text-blue-500"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {/* Your SVG code here */}
+              {/* Example: */}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+
     </div>
   );
 }
