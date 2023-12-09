49bb04b2ee5684b2232855bf8f9322b085a03824
KritikaJoshi22 <joshikritika102@gmail.com>
2023-12-09 12:33:09 +0530

    Updated contract and added openzeppiln

--- src/Home.js
+++ src/Home.js
@@ -1,30 +1,31 @@
 // src/components/LandingPage.js
 
-import React, {useRef} from 'react';
-import researchPaperImage from './research.svg';
-import { Link } from 'react-router-dom';
+import React, { useRef } from "react";
+import researchPaperImage from "./research.svg";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-
-
   return (
     <div className="bg-gray-900 min-h-screen flex items-center justify-center text-white">
-        <div className="flex-1 p-36">
-          <h1 className="text-4xl font-bold mb-6">Researcher Platform</h1>
-          <p className="text-lg mb-8 ">
-            Welcome to our platform where you can upload and manage your research papers in a completely decentralized way no hustle of waiting for so long to get your work published and verified with the power of blockchain to own and ai to check uniqueness and relations
-          </p>
-          <button  className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-           <Link to="/upload">Upload Research</Link> 
-          </button>
-          <button className="bg-blue-500 ml-4 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-            <Link to= "/reviewer">Review Researches</Link>
-          </button>
-        </div>
-        <div className="flex-1 flex  items-center">
-          <img src={researchPaperImage} alt="Research Paper" className="h-100" />
-          {/* Adjust the height (h-40) and styles according to your SVG */}
-        </div>
+      <div className="flex-1 p-36">
+        <h1 className="text-4xl font-bold mb-6">Researcher Platform</h1>
+        <p className="text-lg mb-8 ">
+          Welcome to our platform where you can upload and manage your research
+          papers in a completely decentralized way no hustle of waiting for so
+          long to get your work published and verified with the power of
+          blockchain to own and ai to check uniqueness and relations.
+        </p>
+        <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+          <Link to="/upload">Upload Research</Link>
+        </button>
+        <button className="bg-blue-500 ml-4 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+          Review Research
+        </button>
+      </div>
+      <div className="flex-1 flex  items-center">
+        <img src={researchPaperImage} alt="Research Paper" className="h-100" />
+        {/* Adjust the height (h-40) and styles according to your SVG */}
+      </div>
     </div>
   );
 };
--- upload.sol
+++ upload.sol
@@ -7,83 +7,105 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
-contract submissionNFT is ERC721, ERC721URIStorage, Ownable, ERC721Enumerable, ERC721Burnable{
+contract SubmissionNFT is ERC721, ERC721URIStorage, Ownable, ERC721Enumerable, ERC721Burnable {
 
     uint256 public mintAmt;
+
+    // Mapping to store user types
+    mapping(address => bool) public researchers;
+    mapping(address => bool) public reviewers;
+
     constructor(
         string memory _name,
-        string memory _symbol, 
+        string memory _symbol,
         address _owner
-        ) 
-        ERC721(_name, _symbol)
-        Ownable(_owner){
+    )
+    ERC721(_name, _symbol)
+    Ownable(_owner)
+    {
     }
 
     function _mintNFT(
-        address _to, 
+        address _to,
         string memory _tokenURI
-        )external payable onlyOwner{ 
-            
-            require (msg.value >= mintAmt, "insufficient funds");
-            uint256 tokenId = totalSupply();
-            _mint(_to, tokenId);
-            _setTokenURI(tokenId, _tokenURI);
+    )
+    external payable onlyOwner
+    {
+        require(msg.value >= mintAmt, "Insufficient funds");
+        uint256 tokenId = totalSupply();
+        _mint(_to, tokenId);
+        _setTokenURI(tokenId, _tokenURI);
+    }
 
+    function transfer(address receiver, uint256 amount)
+    public
+    virtual
+    {
+        _transfer(msg.sender, receiver, amount);
+    }
+
+    function setMintNFTAmt(uint256 _amt) external onlyOwner {
+        mintAmt = _amt;
+    }
+
+    // Function to check if the user is a researcher
+    function checkResearcher() public view returns (bool) {
+        return researchers[msg.sender];
+    }
+
+    // Function to check if the user is a reviewer
+    function checkReviewer() public view returns (bool) {
+        return reviewers[msg.sender];
+    }
+
+    // Function to choose a role if not a researcher or reviewer
+    function chooseRole(bool _isResearcher) public {
+        require(!researchers[msg.sender] && !reviewers[msg.sender], "Already assigned a role");
+
+        if (_isResearcher) {
+            researchers[msg.sender] = true;
+        } else {
+            reviewers[msg.sender] = true;
         }
+    }
 
-        function transfer(address reciever, uint256 amount)
-        public
-        virtual 
-        {
+    // Override tokenURI function
+    function tokenURI(uint256 tokenId)
+    public
+    view
+    virtual
+    override(ERC721, ERC721URIStorage)
+    returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
 
-            _transfer(msg.sender, reciever, amount);
+    // Override supportsInterface function
+    function supportsInterface(bytes4 interfaceId)
+    public
+    view
+    override(ERC721, ERC721URIStorage, ERC721Enumerable)
+    returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
 
-        }
-        
-        function setMintNFTAmt(uint256 _amt) external onlyOwner{
-            mintAmt = _amt;
-        }
+    // Override _update function
+    function _update(address to, uint256 tokenId, address auth)
+    internal
+    virtual
+    override(ERC721, ERC721Enumerable)
+    returns (address)
+    {
+        return super._update(to, tokenId, auth);
+    }
 
-         //The following functions are overrides
-
-        function tokenURI(
-            uint256 tokenId
-        )
-        public 
-        view 
-        virtual
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-        {
-
-            return super.tokenURI(tokenId);
-        }
-
-        function supportsInterface(bytes4 interfaceId)
-        public 
-        view
-        override(ERC721, ERC721URIStorage, ERC721Enumerable)
-        returns(bool)
-        {
-
-            return super.supportsInterface(interfaceId);
-
-        }
-
-        function _update(address to, uint256 tokenId, address auth)
-        internal
-        virtual
-        override(ERC721, ERC721Enumerable)
-        returns(address){
-
-            return super._update(to, tokenId, auth);
-        }
-
-         function _increaseBalance(address account, uint128 value) 
-         internal 
-         virtual 
-         override(ERC721, ERC721Enumerable){ 
-            super._increaseBalance(account, value);
-         }
-
+    // Override _increaseBalance function
+    function _increaseBalance(address account, uint128 value)
+    internal
+    virtual
+    override(ERC721, ERC721Enumerable)
+    {
+        super._increaseBalance(account, value);
+    }
 }
