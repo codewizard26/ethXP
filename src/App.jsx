3e091e9bfd308a5543cd00bb0bd328c9e3cbbb87
infoparth <parthatharv2606@gmail.com>
2023-12-09 12:08:34 +0530

    push protocol

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import {
@@ -6,16 +6,55 @@ import {
   useAnonAadhaar,
   AnonAadhaarProof,
 } from "anon-aadhaar-react";
+import { useSDK } from "@metamask/sdk-react";
+import { ethers } from "ethers";
+import PushMessage from "./pushImplementation";
 
 function App() {
+  const [account, setAccount] = useState(null);
+  const [signer, setSigner] = useState(null);
+  const [signerAddress, setSignerAddress] = useState(null);
+
+  const { sdk, connected, connecting, provider, chainId } = useSDK();
   const [anonAadhaar] = useAnonAadhaar();
+
+  const connect = async () => {
+    try {
+      const accounts = await sdk?.connect();
+      console.log(`accounts`, accounts);
+      setAccount(accounts?.[0]);
+      const _provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = _provider.getSigner();
+      const signerAddress = await signer.getAddress();
+      setSigner(signer);
+      setSignerAddress(signerAddress);
+    } catch (err) {
+      console.warn(`failed to connect..`, err);
+    }
+  };
 
   useEffect(() => {
     console.log("Anon Aadhaar status: ", anonAadhaar.status);
-  }, [anonAadhaar]);
+    console.log("sdk connected: ", connected);
+  }, [anonAadhaar, connected]);
 
   return (
     <div>
+      <div className="Wallet Connect">
+        {/* <MetaMaskButton theme={"light"} color="white"></MetaMaskButton> */}
+        <div className="App">
+          <button onClick={connect}>Connect Wallet</button>
+          {connected && (
+            <div>
+              <>
+                {chainId && `Connected chain: ${chainId}`}
+                <p></p>
+                {account && `Connected account: ${account}`}
+              </>
+            </div>
+          )}
+        </div>
+      </div>
       <div>
         <LogInWithAnonAadhaar />
         <p>{anonAadhaar?.status}</p>
@@ -29,6 +68,7 @@ function App() {
           </>
         )}
       </div>
+      <PushMessage signer={signer} clicked={true} />
     </div>
   );
 }
